86cb88d4699d4c712d73990053a1ca691091a65c
ivan <imysin@mail.ru>
2026-07-18 08:12:02 +0300

    Работаем с оптимизицией начальных условий симуляции

--- train_wc.py
+++ train_wc.py
@@ -348,8 +348,8 @@ class WilsonCowanNetwork(Layer):
         h = self.dt_dim
 
         # Presynaptic firing rate → spikes/ms (Hz * 0.001 = kHz).
-        FRpre_unit = E * 0.001 * h
-        FRpre_ext = inputs * 0.001 * h
+        FRpre_unit = E * h  #!! 0.001 *
+        FRpre_ext = inputs * h #!! 0.001 *
         if self.pre == self.units:
             FRpre = FRpre_ext
         else:
